4b71642565ccf043fc82524545e3769444c0d574
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-05-16 16:18:39 +0700

    optimization_code

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,14 +2,14 @@ from element_locators import auth_elements as el
 from page.login_app import LoginApp
 
 
-def test_registration_in_phone_to_russian(driver_android):
+def test_registration_in_phone_to_russian_on_logout(driver_android):
     '''Регистрация через телефон для России, после логуат'''
     app_login = LoginApp(driver_android)
     app_login.registration(code_phone=el.russian_code, country=el.russia, phone=el.rus_kz_phone)
     app_login.exit()
 
 
-def test_auth_in_phone_to_russian(driver_android):
+def test_auth_in_phone_to_russian_on_delete(driver_android):
     '''Авторизация через телефон для России, после удаление аккаунта'''
     app_login = LoginApp(driver_android)
     app_login.auth(code_phone=el.russian_code, country=el.russia, phone=el.rus_kz_phone)
@@ -18,21 +18,21 @@ def test_auth_in_phone_to_russian(driver_android):
     app_login.delete_user()
 
 
-def test_registration_in_phone_to_belarussia(driver_android):
+def test_registration_in_phone_to_belarussia_on_delete(driver_android):
     '''Регистрация через телефон для Беларуссии, после удаление аккаунта'''
     app_login = LoginApp(driver_android)
     app_login.registration(code_phone=el.belarussia_code, country=el.belarussia, phone=el.bld_kgz_phone)
     app_login.delete_user()
 
 
-def test_registration_in_phone_to_kazahstan(driver_android):
+def test_registration_in_phone_to_kazahstan_on_delete(driver_android):
     '''Регистрация через телефон для Казахстана, после удаление аккаунта'''
     app_login = LoginApp(driver_android)
     app_login.registration(code_phone=el.kazahstan_code, country=el.kazahstan, phone=el.rus_kz_phone)
     app_login.delete_user()
 
 
-def test_registration_in_phone_to_kirgistan(driver_android):
+def test_registration_in_phone_to_kirgistan_on_delete(driver_android):
     '''Регистрация через телефон для Киргизии, после удаление аккаунта'''
     app_login = LoginApp(driver_android)
     app_login.registration(code_phone=el.kirgistan_code, country=el.kirgistan, phone=el.bld_kgz_phone)
@@ -41,7 +41,7 @@ def test_registration_in_phone_to_kirgistan(driver_android):
 
 
 def test_list_country_in_doc(driver_android):
-    '''Проверка отображения всех стран в разделе документов'''
+    '''Проверка отображения содержимого раздела документов'''
     app_login = LoginApp(driver_android)
     app_login.registration(code_phone=el.russian_code, country=el.russia, phone=el.rus_kz_phone)
     app_login.documents()
