bb7470fb0ffbd842fa20ec75a33ebab3c7e1ba4a
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-06-01 12:11:20 +0700

    pep8

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,7 +19,10 @@ appium_server_url = 'http://localhost:4723'
 
 @pytest.fixture()
 def driver_android():
-    app_driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities_android))
+    app_driver = webdriver.Remote(
+        appium_server_url,
+        options=UiAutomator2Options().load_capabilities(capabilities_android)
+    )
     yield app_driver
     if app_driver.session_id:
         app_driver.quit()
