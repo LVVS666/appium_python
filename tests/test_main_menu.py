170ea2d646a77a723598e8319ac33357a499ed4b
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-06-27 11:09:18 +0700

    schema_menu

--- tests/test_main_menu.py
+++ tests/test_main_menu.py
@@ -1,7 +1,7 @@
 from element_locators import menu_elements as menu_el
 
 
-def test_not_auth():
+def test_not_auth_menu():
     '''Проверка разделов бокового меню у неавторизованного пользователя'''
     pass
 
