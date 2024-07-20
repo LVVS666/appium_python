36d91a79ec39d7b73de187f2fadffd823b8285e2
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2024-07-20 19:50:50 +0700

    api_xiaomi_create

--- fixtures/xiaomi_api_requests.py
+++ fixtures/xiaomi_api_requests.py
@@ -1,40 +1,51 @@
+import hashlib
+import hmac
+import json
 import requests
 
 
 class XiaomiApi:
-    base_url = 'https://stageapi.berizaryad.ru/auth/api/v2'
-    signature = 'b535be8d7d730c4d2c43bb822c49417cfce43a357aae759d62271f0811049ad0'
-    headers = {
-        'Content-Type': 'application/json',
-        'signature': signature
+    base_url = 'https://stagemobileapi.berizaryad.ru/auth/api/v2'
+    signature_key = None #Добавить env файл с приватным ключом
+    xiaomi_payload = {
+        "app_version": "5.5.0",
+        "os_version": "34",
+        "platform": "Android",
+        "udid": "9431f444e5844793"
     }
 
-    def xiaomi_api_login(self, number):
-        url = "/login"
-        xiaomi_login_payload = {
-            "app_version": "5.5.0",
-            "os_version": "34",
-            "phone": number,
-            "platform": "Android",
-            "udid": "9431f444e5844793"
-        }
-        return requests.post(url=self.base_url+url, params=xiaomi_login_payload, headers=self.headers)
+    def generate_signature(self, raw_data):
+        '''Генерация сигнатуры'''
+        msg = json.dumps(raw_data).encode()
+        messenger_key = self.signature_key.encode()
+        signature = hmac.new(messenger_key, msg, digestmod=hashlib.sha256).hexdigest()
+        return {'Locale': 'ru', 'signature': signature, 'Content-Type': 'application/json'}
 
+    def xiaomi_api_login(self, number):
+        '''Запрос на логин'''
+        url = "/login"
+        data_json = self.xiaomi_payload
+        data_json['phone'] = number
+        return requests.post(url=self.base_url+url, json=data_json, headers=self.generate_signature(data_json))
 
     def xiaomi_api_confirm(self, number):
+        '''Запрос на ввод смс кода'''
         url = "/confirm"
-        xiaomi_confirm_payload = {
-            "app_version": "5.5.0",
-            "code": "0001",
-            "os_version": "34",
-            "phone": number,
-            "platform": "Android",
-            "udid": "9431f444e5844793"
+        data_json = self.xiaomi_payload
+        data_json['phone'] = number
+        data_json['code'] = "0001"
+        return requests.post(url=self.base_url+url, json=data_json, headers=self.generate_signature(data_json))
+
+    def xiaomi_api_users(self, token):
+        '''Запрос на получение информации о пользователе'''
+        url = "/users"
+        headers = {
+            'Authorization': f'Bearer {token}'
         }
-        response_data = requests.post(url=self.base_url+url, params=xiaomi_confirm_payload, headers=self.headers)
-        return response_data.json()
+        return requests.get(url=self.base_url+url, headers=headers)
 
 
 kl = XiaomiApi()
 kl.xiaomi_api_login("79992769943")
-print(kl.xiaomi_api_confirm("79992769943"))
+token = kl.xiaomi_api_confirm("79992769943").json()
+print(kl.xiaomi_api_users(token['access_token']))
